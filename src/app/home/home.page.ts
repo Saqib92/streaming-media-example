6889b2f8521e1909edc0d8c86163bd7591304b68
saqib92 <najam1992@gmail.com>
2019-11-15 15:39:14 +0500

    Streaming Media Example project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private streamingMedia: StreamingMedia) {}
+
+// We Need this.streamingMedia.clsoeVideo(); // this functionality to close player programatically. Please help 
+
+  playStreaming(url){
+  	let options: StreamingVideoOptions = {
+	  successCallback: () => { console.log('Video played') },
+	  errorCallback: (e) => { console.log(e, 'Error streaming') },
+	  orientation: 'landscape',
+	  shouldAutoClose: true,
+	  controls: true
+	};
+
+	this.streamingMedia.playVideo(url, options);
+  }
 
 }
